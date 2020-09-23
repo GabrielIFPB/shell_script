afa1b7a3ccbf055c8917d141c6d8f7defb8cc830
GabrielIFPB <jeronimogbariel@gmail.com>
2020-09-23 18:46:19 -0300

    opções de fi

--- condicional.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-VAR1=""
-VAR2=""
-
-if [[ "$VAR1" = "$VAR2" ]]; then
-	echo "São iguais."
-fi
--- modulo01/condicional.sh
+++ modulo01/condicional.sh
@@ -6,3 +6,30 @@ VAR2=""
 if [[ "$VAR1" = "$VAR2" ]]; then
 	echo "São iguais."
 fi
+
+if [[ "$VAR1" = "$VAR2" ]] 
+then
+	echo "São iguais."
+fi
+
+if test "$VAR1" = "$VAR2"; then
+	echo "São iguais."
+fi
+
+if test "$VAR1" = "$VAR2" 
+then
+	echo "São iguais."
+fi
+
+if [ "$VAR1" = "$VAR2" ]; then
+	echo "São iguais."
+fi
+
+if [ "$VAR1" = "$VAR2" ] 
+then
+	echo "São iguais."
+fi
+
+[ "$VAR1" = "$VAR2" ] && echo "São iguais." # true
+
+[ "$VAR1" = "$VAR2" ] || echo "São iguais." # false
